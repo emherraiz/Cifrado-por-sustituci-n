7013c4ee74f54c5abd685ea61e2fa8ab08df34a3
Enrique Marina <eherraiz@myuax.com>
2023-02-07 20:01:14 +0100

    seguimos

---
+++ atbash.py
@@ -0,0 +1,25 @@
+""" https://en.wikipedia.org/wiki/Atbash """
+import string
+
+def atbash(sequence: str) -> str:
+    """
+    >>> atbash("ABCDEFG")
+    'ZYXWVUT'
+
+    >>> atbash("aW;;123BX")
+    'zD;;123YC'
+    """
+    letters = string.ascii_letters
+    letters_reversed = string.ascii_lowercase[::-1] + string.ascii_uppercase[::-1]
+    return "".join(
+        letters_reversed[letters.index(c)] if c in letters else c for c in sequence
+    )
+
+
+
+if __name__ == "__main__":
+    Texto_encriptado = 
+    print(f'Texto encriptado: ')
+
+
+# Codigo sacado de https://the-algorithms.com/es/algorithm/atbash
